1966c706bacdb6d035d5610d8d3a7abb40d60e7e
Joe Gretton <joegretton2010@hotmail.com>
2019-11-14 02:17:15 +0000

    Add run.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,49 +3,50 @@ import json
 import random
 from flask import Flask, render_template, jsonify
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        # DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
-    
-    """     if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+# def create_app(test_config=None):
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass """
+# create and configure the app
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    # DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+)
 
-    @app.route('/')
-    def home():
-        return render_template('index.html')
+"""     if test_config is None:
+    # load the instance config, if it exists, when not testing
+    app.config.from_pyfile('config.py', silent=True)
+else:
+    # load the test config if passed in
+    app.config.from_mapping(test_config)
 
-    @app.route('/sudoku')
-    def sudoku():
-        return render_template('sudoku.html')
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass """
 
-    @app.route('/about')
-    def about():
-        return render_template('about.html')
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-    @app.route('/data/sudokus')
-    def data_sudokus():
-        with open('sudokus.json') as jsonfile:
-            sudokus = []
-            for line in jsonfile:
-                data = {}
-                data['sudoku'] = json.loads(line)
-                sudokus.append(data)
+@app.route('/sudoku')
+def sudoku():
+    return render_template('sudoku.html')
 
-            sudoku = random.choice(sudokus)
-            return jsonify(sudoku)
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
-    return app
+@app.route('/data/sudokus')
+def data_sudokus():
+    with open('sudokus.json') as jsonfile:
+        sudokus = []
+        for line in jsonfile:
+            data = {}
+            data['sudoku'] = json.loads(line)
+            sudokus.append(data)
+
+        sudoku = random.choice(sudokus)
+        return jsonify(sudoku)
+
+#    return app
