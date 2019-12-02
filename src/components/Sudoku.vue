456675ba70f5f2f9149364a387ca34cf37e44c77
Joe Gretton <joegretton2010@hotmail.com>
2019-12-02 00:21:15 +0000

    Update Sudoku.vue

--- src/components/Sudoku.vue
+++ src/components/Sudoku.vue
@@ -2,10 +2,13 @@
     <div id='sudoku'>
         <table id='grid' class='disable-select' draggable='false'>
             <tr v-for='(row, i) in sudoku' :key='row.id' :id='"r-" + i'>
-                <td v-for='(cell, j) in row' :key='cell.id' :id='"c-" + j' @click="select_cell(i, j)" class='cell'>{{ sudoku[i][j] }}</td>
+                <td v-for='(cell, j) in row' :key='cell.id' :id='"c-" + j' class='cell'
+                    :class='{ highlight: is_cell_selected(i, j) }'
+                    @click="toggle_cell_highlight(i, j)">
+                        {{ sudoku[i][j] }}
+                </td>
             </tr>
         </table>
-        <p>{{ selected_cells }}</p>
     </div>
 </template>
 
@@ -39,28 +42,38 @@ export default {
             }
             return sudoku
         },
-        select_cell(i, j) {
-            console.log(this.is_cell_selected(i, j))
-            if(this.is_cell_selected(i, j) == true) {
-                console.log('Pop')
-                return
+        toggle_cell_highlight(i, j) {
+            // Find if selected cell is already highlighted
+            let cell_index = this.selected_cells.findIndex(cell => cell.i == i && cell.j == j)
+            if(cell_index >= 0) {
+
+                this.selected_cells.splice(cell_index, 1)
+            } else {
+                let coord = {"i": i, "j": j}
+                this.selected_cells.push(coord)
             }
-            let coord = {"i": i, "j": j}
-            this.selected_cells.push(coord)
         },
         is_cell_selected(i, j) {
-            this.selected_cells.forEach(function(cell) {
-                console.log(cell)
-                if(cell.i == i || cell.j == j) {
-                    console.log('TRUE')
-                    return
-                }
-            })
+            let cell_index = this.selected_cells.findIndex(cell => cell.i == i && cell.j == j)
+            if(cell_index > -1) {
+                return true
+            }
             return false
+        },
+        runfunction(e) {
+            console.log('up')
         }
     },
     created: function() {
         this.sudoku = this.empty_sudoku()
+        
+        window.addEventListener('keydown', function(e) {
+            if (e.key >= 0 && e.key <= 9) {
+                console.log(this.selected_cells)
+                // Enter number into selected cells
+                
+            }
+        })
     }
 }
 
@@ -76,4 +89,8 @@ export default {
     #grid td:first-child { border-left: solid 4px; }
     #grid td:last-child { border-right: solid 4px; }
     .cell { display: inline-block; cursor: pointer; border: solid black; border-width: 0px 1px 1px 0px; width: 50px; height: 50px; text-align: center; font-size: 34px; }
+
+    .highlight {
+        background: yellow;
+    }
 </style>
